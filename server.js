4abcca64184e5cf8429ea84bef0d424e20aa46e3
Braeden Ferguson <bferguson658@gmail.com>
2023-11-12 21:09:15 -0500

    Add Save Function and I/O

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ Date: November 12, 2023
 
 const express = require('express');
 const cors = require('cors');
+const { DateTime } = require('luxon');
+const fs = require('fs');
 const app = express();
 const PORT = 5001;
+
+
 
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
@@ -20,9 +24,42 @@ app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
 
+try {
+    const data = fs.readFileSync('schedule.json');
+    tasks = JSON.parse(data);
+    console.log('Schedule loaded successfully.');
+  } catch (err) {
+    console.error('Error loading schedule:', err.message);
+    //create empty array if file loading of schedule.json fails
+    tasks = [];
+    console.log('Created empty schedule');
+  }
+
+
+  
+  // Save tasks to JSON file
+  const saveSchedule = () => {
+    fs.writeFileSync('schedule.json', JSON.stringify(tasks), 'utf-8');
+    console.log('Schedule saved successfully.');
+  };
+
+  // Middleware to save schedule on every request
+app.use((req, res, next) => {
+    saveSchedule();
+    next();
+  });
+
+
 //creates a new task based on the CRUD opperations in App.js
 app.post('/tasks', (req, res) => {
-    const newTask = { id: Date.now(), text: req.body.text, completed: false };
+    const { text, dueDate } = req.body;
+    
+    const newTask = { 
+        id: Date.now(), 
+        text: req.body.text, 
+        completed: false,
+        dueDate: dueDate ? DateTime.fromISO(dueDate, { zone: 'America/New_York' }).toISO() : null,
+    };
     tasks.push(newTask);
     res.json(newTask);
 });
